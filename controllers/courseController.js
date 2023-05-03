7e5b54088c2296471ff1bbc0308fe4cac9a43548
busrakk <busrakartal160@gmail.com>
2023-05-03 14:45:34 +0300

    add courses listing

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,25 @@
 const Course = require('../models/Course'); // course model
 
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).render('courses', {
+      courses,
+      page_name: 'courses',
+    });
+    // res.status(200).json({
+    //   status: 'success',
+    //   courses,
+    // }); // for postman
+  } catch (error) {
+    console.log(error);
+    // res.status(400).json({
+    //   status: 'fail',
+    //   error,
+    // }); // for postman
+  }
+};
+
 exports.createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
@@ -7,7 +27,7 @@ exports.createCourse = async (req, res) => {
       status: 'success',
       course,
     }); // yeni oluşturulduğu için 201 kodu
-  } catch {
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
