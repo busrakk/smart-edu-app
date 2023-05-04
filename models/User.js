66024ffa78c69ee864760fb374902668cf0e1da3
busrakk <busrakartal160@gmail.com>
2023-05-04 13:54:01 +0300

    install bcrypt to hash passwords

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -15,6 +16,16 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+// model içinde middleware oluşturmak için 'pre' methodu kullanıldı.
+// password alanını şifreleyip "hash" e çevirme ve veritabanımına şifrelenmiş şekilde kaydedtme
+UserSchema.pre('save', function (next) {
+  const user = this; // giriş yapan kullanıcıyı yakalamak için
+  bcrypt.hash(user.password, 10, (error, hash) => {
+    user.password = hash; // password, hash olarak kaydetme
+    next();
+  });
 });
 
 const User = mongoose.model('User', UserSchema);
