01a7ac3791f7413f24b5f52be15f3616596f5dbd
busrakk <busrakartal160@gmail.com>
2023-05-04 15:55:49 +0300

    add login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,26 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body; // kullanıcının girdiği girdiler
+    const user = await User.findOne({ email }); // emailden kullanıcı bulma
+    if (user) { // eğer kullanıcı varsa
+      bcrypt.compare(password, user.password, (error, same) => { // girilen şifre ile kayıtlı kullanıcı şifresini karşılaştırma
+        if (same) { // şifreler aynıysa
+          // user session
+          res.status(200).send('success');
+        }
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
