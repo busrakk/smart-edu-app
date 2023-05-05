b6a497ef265862f20eb6dba8fbe692c06e6bc93e
busrakk <busrakartal160@gmail.com>
2023-05-05 19:13:04 +0300

    add user roles

--- models/User.js
+++ models/User.js
@@ -16,6 +16,11 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ['admin', 'teacher', 'student'],
+    default: 'student',
+  },
 });
 
 // model içinde middleware oluşturmak için 'pre' methodu kullanıldı.
