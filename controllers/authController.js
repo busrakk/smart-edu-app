275b85931bfcca417fb81590495b77baa4fd81cf
busrakk <busrakartal160@gmail.com>
2023-05-05 09:47:17 +0300

    add dashboard page

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,13 +20,16 @@ exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body; // kullanıcının girdiği girdiler
     const user = await User.findOne({ email }); // emailden kullanıcı bulma
-    if (user) { // eğer kullanıcı varsa
-      bcrypt.compare(password, user.password, (error, same) => { // girilen şifre ile kayıtlı kullanıcı şifresini karşılaştırma
-        if (same) { // şifreler aynıysa
+    if (user) {
+      // eğer kullanıcı varsa
+      bcrypt.compare(password, user.password, (error, same) => {
+        // girilen şifre ile kayıtlı kullanıcı şifresini karşılaştırma
+        if (same) {
+          // şifreler aynıysa
           // user session
           // oturum açan kullanıcıyı belirlemek için.
           req.session.userID = user._id; // giriş işlemi sırasında kullanıcı ID'sini session alanında userID değişkenini oluşturup ona atama
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       });
     }
@@ -41,4 +44,12 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy();
   res.status(200).redirect('/');
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID }); // giriş yapan kullanıcıyı bulma
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
+  });
 };
