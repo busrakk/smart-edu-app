6d3d5fb251865277d925764390d0f9ee9c61c693
busrakk <busrakartal160@gmail.com>
2023-05-07 16:32:59 +0300

    add and delete categories for admin

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,25 @@ const Category = require('../models/Category');
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      category,
-    });
+    res.status(201).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id);
+
+    req.flash('success', `${category.name} has been removed successfully`);
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    }); // bad request için 400 kodu
   }
 };
